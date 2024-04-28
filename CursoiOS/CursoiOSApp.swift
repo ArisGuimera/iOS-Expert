e0bd1a3fdf474be4b9978edeaab685da1fa29c14
AristiDevs <aris.guimera@gmail.com>
2024-04-28 15:26:05 +0100

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Arístides Guimerá Orozco on 28/4/24.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
